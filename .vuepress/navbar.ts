c02ba15b7247686148a651c57b48a06a23076bc2
软件学习 <2840283719@qq.com>
2023-12-17 13:50:17 +0800

    update

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -5,4 +5,19 @@ export default [
         text: "学习路线",
         link: '/学习路线/'
     },
+    {
+        text: "大学学习资料",
+        link: '/大学学习资料/',
+        items: [
+            {
+                text: "虚拟化", link: "/大学学习资料/#虚拟化",
+            },
+            {
+                text: "云计算", link: "/大学学习资料/#云计算",
+            },
+
+        ]
+
+    },
+
 ] as NavItem[];
